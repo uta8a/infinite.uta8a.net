d143f29a7d9e990c7b53e5891e288a2b36774e54
Masanori Tani <uta8a@uta8a.org>
2023-01-20 08:15:33 +0900

    wip

--- _config.ts
+++ _config.ts
@@ -1,7 +1,14 @@
 import lume from "lume/mod.ts";
+import basePath from "lume/plugins/base_path.ts";
+import blog from "https://raw.githubusercontent.com/uta8a/theme-simple-blog-for-me/for-infinite-uta8a-net/mod.ts";
+import imagick from "lume/plugins/imagick.ts";
 
 const site = lume({
   location: new URL("https://infinite.uta8a.net/")
 });
+
+site.use(blog());
+site.use(basePath());
+site.use(imagick());
 
 export default site;
---
+++ about.md
@@ -0,0 +1,16 @@
+---
+layout: layouts/page.njk
+title: About
+menu:
+  visible: true
+  order: 2
+---
+
+[@uta8a](https://github.com/uta8a) (Masanori Tani) と申します。技術記事を読むのが好きです。
+
+私に対して、以下の連絡手段をとることができます。
+
+- [GitHub](https://github.com/uta8a) からメールアドレスに向けてEmailを送っていただく
+- [Google Form](https://forms.gle/XUxYCH3rra7u4Mk79) からご連絡
+
+気軽に「この記事僕読んだのですがおすすめでした！」とかでも大丈夫です。ドシドシご連絡お願いします。
